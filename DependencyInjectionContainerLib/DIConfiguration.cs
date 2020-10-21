e95f54e8f695764e4a6ff95d5aafa27cea9391b6
Den25570 <44478211+Den25570@users.noreply.github.com>
2020-10-21 04:37:08 +0300

    add basic implementation of di configuration & container

---
+++ DependencyInjectionContainerLib/DIConfiguration.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionContainerLib
+{
+    public class DIConfiguration
+    { 
+    // Bool value is true if object lifetime type is Singletone
+    public Dictionary<Type, bool> lifetimeSettings;
+
+    // Dictionary to story created objects for sigletone types
+    public Dictionary<Type, SingletonContainer> objectContainer;
+
+    // Dictionary to map Abstracy Dependency to Concrete Implementation(s)
+    public Dictionary<Type, List<Type>> dependenciesContainer;
+
+
+    public void Register<TDependency, TImplementation>(bool isSingleton = false)
+    {
+        Type tDependency = typeof(TDependency);
+        Type tImplementation = typeof(TImplementation);
+
+        // Register dependency in a dictionary
+        if (!dependenciesContainer.ContainsKey(tDependency))
+        {
+            dependenciesContainer[tDependency] = new List<Type>();
+            dependenciesContainer[tDependency].Add(tImplementation);
+        }
+        else
+        {
+            if (!dependenciesContainer[tDependency].Contains(tImplementation))
+            {
+                dependenciesContainer[tDependency].Add(tImplementation);
+            }
+        }
+
+        // Register type lifetime settings
+        lifetimeSettings[tImplementation] = isSingleton;
+
+        // Register and type in the object storage
+        if (isSingleton)
+        {
+            objectContainer[tImplementation] = new SingletonContainer();
+        }
+    }
+
+    public void Register(Type tDependency, Type tImplementation)
+    {
+        if (!dependenciesContainer.ContainsKey(tDependency))
+        {
+            dependenciesContainer[tDependency] = new List<Type>();
+            dependenciesContainer[tDependency].Add(tImplementation);
+        }
+        else
+        {
+            // Implementations in array must be unique
+            if (!dependenciesContainer[tDependency].Contains(tImplementation))
+            {
+                dependenciesContainer[tDependency].Add(tImplementation);
+            }
+        }
+    }
+
+    public DIConfiguration()
+    {
+        lifetimeSettings = new Dictionary<Type, bool>();
+        objectContainer = new Dictionary<Type, SingletonContainer>();
+        dependenciesContainer = new Dictionary<Type, List<Type>>();
+    }
+}
+}
---
+++ DependencyInjectionContainerLib/DIContainer.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionContainerLib
+{
+    public class DIContainer
+    {
+        DIConfiguration configuration;
+
+        public DIContainer(DIConfiguration configuration)
+        {
+            validateConfiguration(configuration);
+            this.configuration = configuration;
+        }
+
+        public void validateConfiguration(DIConfiguration configuration)
+        {
+            foreach (Type tDependency in configuration.dependenciesContainer.Keys)
+            {
+                if (tDependency.IsValueType)
+                {
+                    throw new ArgumentException("TDependency must be a reference type");
+                }
+
+                foreach (Type tImplementation in configuration.dependenciesContainer[tDependency])
+                {
+                    // Checks if TImplementation inherits from/implements TDependency
+                    if (!tDependency.IsAssignableFrom(tImplementation) && !tDependency.IsGenericTypeDefinition)
+                    {
+                        throw new ArgumentException("TImplementation must be inherited from/implement Dependency type.");
+                    }
+
+                    if (tImplementation.IsAbstract || !tImplementation.IsClass)
+                    {
+                        throw new ArgumentException("TImplementation must be a non-abstract class");
+                    }
+                }
+            }
+        }
+
+        public List<TDependency> Resolve<TDependency>()
+        {
+            Type tDependency = typeof(TDependency);
+            if (tDependency.IsGenericType && tDependency.GetGenericTypeDefinition().Equals(typeof(IEnumerable<>)))
+            {
+                tDependency = tDependency.GenericTypeArguments[0];
+            }
+
+            if (tDependency.IsGenericType)
+            {
+                foreach (var type in tDependency.GenericTypeArguments)
+                {
+                    if (!configuration.dependenciesContainer.ContainsKey(type))
+                    {
+                        throw new KeyNotFoundException($"Dependency {type.ToString()} is not registered.");
+                    }
+                }
+            }
+            else
+            if (!configuration.dependenciesContainer.ContainsKey(tDependency))
+            {
+                throw new KeyNotFoundException($"Dependency {tDependency.ToString()} is not registered.");
+            }
+
+            List<TDependency> result = new List<TDependency>();
+
+            if (tDependency.IsGenericType && configuration.dependenciesContainer.ContainsKey(tDependency.GetGenericTypeDefinition()))
+            {
+
+                var implementations = configuration.dependenciesContainer[tDependency.GetGenericTypeDefinition()];
+                foreach (var implementation in implementations)
+                {
+                    var genericType = implementation.MakeGenericType(tDependency.GetGenericArguments());
+                    var resolved = (TDependency)GetInstance(genericType);
+                    result.Add(resolved);
+                }
+
+                return result;
+
+            }
+            else
+            if (!configuration.dependenciesContainer.ContainsKey(tDependency))
+            {
+                throw new KeyNotFoundException($"Dependency {tDependency.ToString()} is not registered.");
+            }
+
+            foreach (var implementation in configuration.dependenciesContainer[tDependency])
+            {
+                TDependency resolved;
+
+                if (configuration.lifetimeSettings[implementation])
+                {
+                    if (configuration.objectContainer[implementation].instance == null)
+                    {
+                        lock (configuration.objectContainer[implementation].syncRoot)
+                        {
+                            if (configuration.objectContainer[implementation].instance == null)
+                            {
+                                configuration.objectContainer[implementation].instance = GetInstance(implementation);
+                            }
+                        }
+                    }
+
+                    resolved = (TDependency)configuration.objectContainer[implementation].instance;
+
+                }
+                else
+                {
+                    resolved = (TDependency)GetInstance(implementation);
+                }
+
+                result.Add(resolved);
+            }
+
+            return result;
+
+        }
+
+        // Resolve for creating inner dependencies using reflection.
+        public object ResolveFromType(Type t)
+        {
+            var resolveMethod = typeof(DIContainer).GetMethod("Resolve");
+            var resolveType = resolveMethod.MakeGenericMethod(t);
+
+            var listType = typeof(List<>);
+            var constructedListType = listType.MakeGenericType(t);
+
+            dynamic resolved = Convert.ChangeType(resolveType.Invoke(this, null), constructedListType);
+
+            if (resolved.Count > 1)
+            {
+                return resolved;
+            }
+            else
+            {
+                return resolved[0];
+            }
+        }
+
+        private object GetInstance(Type t)
+        {
+            ConstructorInfo constructor = t.GetConstructors().OrderBy(x => x.GetParameters().Length).Last();
+
+            if (constructor != null)
+            {
+                ParameterInfo[] parameters = constructor.GetParameters();
+                object[] parameterInstances = new object[parameters.Length];
+
+                int index = 0;
+                foreach (var param in parameters)
+                {
+
+                    Type paramType = param.ParameterType;
+
+                    if (paramType.IsGenericType && paramType.GetGenericTypeDefinition().Equals(typeof(IEnumerable<>)))
+                    {
+                        paramType = paramType.GenericTypeArguments[0];
+                    }
+
+                    if (paramType.IsGenericType)
+                    {
+                        throw new NotImplementedException();
+                    }
+                    else
+                    {
+                        if (configuration.dependenciesContainer.ContainsKey(paramType))
+                        {
+                            parameterInstances[index] = ResolveFromType(paramType);
+                        }
+                    }
+
+                    index++;
+                }
+
+                var instance = constructor.Invoke(parameterInstances);
+                return instance;
+
+            }
+            else
+            {
+                throw new InvalidOperationException($"No public constructors available for {t}");
+            }
+
+        }
+    }
+}
